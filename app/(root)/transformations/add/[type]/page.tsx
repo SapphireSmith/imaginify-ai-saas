c6bd3b826e76691fee0e88e8830cce7409fb3253
Sapphire Smith <ssapphire527@gmail.com>
2024-10-30 21:24:12 +0530

    transformation page forms creation first step completed

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import Header from '@/components/shared/Header';
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants';
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import React from 'react';
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({ params }: { params: { type: string } }) => {
+
+  const { userId } = await auth()
+
+  const { type } = await params; // Await the params before accessing `type`
+  const transformation = transformationTypes[type as keyof typeof transformationTypes];
+
+  if (!userId) {
+    redirect('/sign-in')
+  }
+  const user = await getUserById(userId)
+
   return (
-    <div>AddTransformationTypePage</div>
-  )
-}
+    <>
+      <Header
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+      <TransformationForm
+        action='Add'
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
+  );
+};
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
---
+++ components/shared/TransformationForm.tsx
@@ -0,0 +1,99 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+// import {
+//   Select,
+//   SelectContent,
+//   SelectGroup,
+//   SelectItem,
+//   SelectLabel,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select"
+
+// import { Button } from "@/components/ui/button"
+import {
+  Form,
+  // FormControl,
+  // FormDescription,
+  // FormField,
+  // FormItem,
+  // FormLabel,
+  // FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { defaultValues } from "@/constants"
+import { CustomField } from "./CustomFields"
+
+
+export const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+})
+
+const TransformationForm = ({ action, data = null, }: TransformationFormProps) => {
+
+  const initialValues = data && action === 'Update' ? {
+    title: data?.title,
+    aspectRatio: data?.aspectRatio,
+    color: data?.color,
+    prompt: data?.prompt,
+    publicId: data?.publicId,
+  } : defaultValues
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <CustomField
+          control={form.control}
+          name="title"
+          formLabel="Image Title"
+          className="w-full"
+          render={({ field }) => <Input {...field} className="input-field" />}
+
+        />
+
+        {/* {type === 'fill' && (
+          <CustomField
+            render={({ field }) => (
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select a fruit" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Fruits</SelectLabel>
+                    <SelectItem value="apple">Apple</SelectItem>
+                    <SelectItem value="banana">Banana</SelectItem>
+                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                    <SelectItem value="grapes">Grapes</SelectItem>
+                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+          />
+        )} */}
+      </form>
+    </Form>
+  )
+}
+
+export default TransformationForm
